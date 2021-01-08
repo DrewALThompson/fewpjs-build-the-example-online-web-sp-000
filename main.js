685fa30c1677f3bfd9697f30ad40d6d66c24da33
Drew Thompson <drewallynthompson@gmail.com>
2021-01-08 07:50:31 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const FULL_HEART = '♥';
 
 const errorModal = document.getElementById('modal');
 const likes = document.querySelectorAll('.like-glyph');
+errorModal.classList.add('hidden');
 
 document.addEventListener('DOMContentLoaded', () => {
-  errorModal.classList.add('hidden');
   
   likes.addEventListener('click',(e) => {
     
